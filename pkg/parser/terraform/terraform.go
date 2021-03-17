cdeb6b46763379f192543821ffbbf382a343c3e8
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-03-17 13:10:49 +0000

    Added resolve to interface and extract variables from terraform.tfvars files
    Signed-off-by: Felipe Avelar <felipe.avelar@checkmarx.com>

--- pkg/parser/terraform/terraform.go
+++ pkg/parser/terraform/terraform.go
@@ -1,13 +1,19 @@
 package terraform
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 
 	"github.com/Checkmarx/kics/pkg/model"
 	"github.com/Checkmarx/kics/pkg/parser/terraform/converter"
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/zclconf/go-cty/cty"
+
+	// hcljson "github.com/hashicorp/hcl/v2/json"
 	"github.com/pkg/errors"
 )
 
@@ -23,12 +29,92 @@ type Parser struct {
 	numOfRetries int
 }
 
+type tfvarsSource struct {
+	value  cty.Value
+	source byte
+}
+
+type inputVariables map[string]tfvarsSource
+
 // NewDefault initializes a parser with Parser default values
 func NewDefault() *Parser {
+	// here comes tf-var and tf-var-files and create Args variables
 	return &Parser{
 		numOfRetries: RetriesDefaultValue,
 		convertFunc:  converter.DefaultConverted,
 	}
+}
+
+// Resolve - replace or modifies in-memory content before parsing
+func (p *Parser) Resolve(fileContent []byte, filename string) (*[]byte, error) {
+	valueMap, _ := getInputVariables(filepath.Dir(filename))
+	validIdentifier := `var\.[0-9a-zA-Z\-_]+(\[[0-9]+\]|\[\"[0-9a-zA-Z\-_]+\"\])*`
+	fullRegexRule := fmt.Sprintf(`(%s)|(\$\{%s\})`, validIdentifier, validIdentifier)
+	regex := regexp.MustCompile(fullRegexRule)
+
+	identifiers := regex.FindAll(fileContent, -1)
+	for _, identifier := range identifiers {
+		keyIdentifier := strings.Replace(string(identifier), "var.", "", -1)
+		if value, ok := valueMap[keyIdentifier]; !ok {
+			fmt.Printf("error")
+		} else {
+			fmt.Printf("%s", value)
+		}
+		fmt.Printf("%q", identifier)
+	}
+	return &fileContent, nil
+}
+
+func getInputVariables(currentPath string) (inputVariables, error) {
+	terraformFilepath := filepath.Join(currentPath, "terraform.tfvars")
+	file, err := os.ReadFile(terraformFilepath)
+	if err != nil {
+		return nil, err
+	}
+	var f *hcl.File
+	if strings.HasSuffix(terraformFilepath, ".json") {
+		// f, _ = hcljson.Parse(file, terraformFilepath)
+		if f == nil || f.Body == nil {
+			return nil, nil
+		}
+	} else {
+		f, _ = hclsyntax.ParseConfig(file, terraformFilepath, hcl.Pos{Line: 1, Column: 1})
+		if f == nil || f.Body == nil {
+			return nil, nil
+		}
+	}
+
+	err = checkTfvarsValid(f, terraformFilepath)
+	if err != nil {
+		return nil, err
+	}
+
+	attrs, _ := f.Body.JustAttributes()
+	iVariables := make(inputVariables, 0)
+	//Here need to change what to get
+	for name, attr := range attrs {
+		val, _ := attr.Expr.Value(nil)
+		iVariables[name] = tfvarsSource{
+			value:  val,
+			source: 't',
+		}
+	}
+	return iVariables, nil
+}
+
+func checkTfvarsValid(f *hcl.File, filename string) error {
+	content, _, _ := f.Body.PartialContent(&hcl.BodySchema{
+		Blocks: []hcl.BlockHeaderSchema{
+			{
+				Type:       "variable",
+				LabelNames: []string{"name"},
+			},
+		},
+	})
+	if len(content.Blocks) > 0 {
+		return fmt.Errorf("failed to get variables from %s, .tfvars file is used to assing values not to declare new variables.", filename)
+	}
+	return nil
 }
 
 // Parse execute parser for the content in a file
